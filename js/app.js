21fcd87223208572ea16ac0da0519590bfc119b5
aoneban <torgregion1@gmail.com>
2023-02-02 22:45:28 +0100

    refactor: add color to button pagination

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const numberOfPhotosPerPage = 24;
 getAPI();
 showPhotos(0, numberOfPhotosPerPage);
 generateButtons();
+listenerForButtons();
 
 async function getAPI() {
   const resp = await fetch(API_URL + API_KEY);
@@ -54,26 +55,39 @@ async function showPhotos(a, b) {
   });
 }
 
-async function generateButtons() {
+async function generateButtons(num) {
   const totalButtons = await totalButtonsPaginations();
   const pagination = document.createElement('div');
   pagination.classList.add('pagination');
   const photos = document.querySelector('.container2');
   for (let i = 1; i <= totalButtons; i++) {
     const btn = document.createElement('button');
-    btn.classList.add('btn');
-    btn.textContent = i;
-    pagination.append(btn);
+    if (num === i) {
+      btn.classList.add('btn');
+      btn.textContent = i;
+      btn.style.backgroundColor = 'red';
+      pagination.append(btn);
+    } else {
+      btn.classList.add('btn');
+      btn.textContent = i;
+      pagination.append(btn);
+    }
   }
   photos.append(pagination);
 }
 
-document.addEventListener('click', async function (event) {
-  if ([...event.target.classList].includes('btn')) {
-    var y = event.target.textContent;
-    var start = numberOfPhotosPerPage * (y - 1);
-    var end = numberOfPhotosPerPage * y;
-    document.querySelector('.photos').innerHTML = '';
-    await showPhotos(start, end);
-  }
-});
+function listenerForButtons() {
+  document.addEventListener('click', async function (event) {
+    if ([...event.target.classList].includes('btn')) {
+      const y = event.target.textContent;
+      const num = Number(y);
+      const start = numberOfPhotosPerPage * (y - 1);
+      const end = numberOfPhotosPerPage * y;
+      const pagination = document.querySelector('.pagination');
+      pagination.remove();
+      document.querySelector('.photos').innerHTML = '';
+      await showPhotos(start, end);
+      generateButtons(num);
+    }
+  });
+}
